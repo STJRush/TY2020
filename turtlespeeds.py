44db7644ad5713c684d7a50a0ec63620b770e5ad
Daniel Murray <daniel.murray@stjosephsrush.ie>
2020-11-26 16:07:43 +0000

    Create turtlespeeds.py

---
+++ turtlespeeds.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+color('red', 'yellow')
+begin_fill()
+
+forward(200)
+left(90)
+write("9 Mb/s")
+
+forward(200)
+left(90)
+write("5 Mb/s")
+
+forward(200)
+left(90)
+write("4 Mb/s")
+
+forward(200)
+left(90)
+write("3 Mb/s")
+
+end_fill()
+done()
